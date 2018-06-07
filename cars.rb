4e119db065121bbdffa181e6b6ca1258bbfa3e83
Jaden <jcottorone21405@gmail.com>
2018-06-07 22:02:18 +0000

    automatically backed up by learn

--- cars.rb
+++ cars.rb
@@ -1,11 +1,31 @@
 class Cars 
  
- def initilize (make, model, color)
+ def initilize (make, model, color = "white")
   @make = make
   @model = model
   @color = color 
-
 end
+
+def make
+  @make
+end
+
+def model 
+  @model
+end
+
+def color
+  @color
 end 
 
-car_1= Cars.new ("Jeep","Grand Cherokee","White")
+def paint= (new_color)
+@color = new_color 
+end 
+
+
+
+
+end 
+
+car_1= Cars.new ("Jeep","Grand Cherokee")
+
