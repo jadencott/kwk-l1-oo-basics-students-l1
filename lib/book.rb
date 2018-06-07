9383c57cd2aa1eb0c5f1916485ff8c9760daf925
Jaden <jcottorone21405@gmail.com>
2018-06-07 22:15:22 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,15 +11,15 @@ def title
 end
 
 
-def author = (author)
-  @author =author 
+def author= (author)
+  @author= author 
 end 
 
 def author 
   @author
 end 
 
-def page_count = (num)
+def page_count= (num)
 @page_count = num
 end 
 
@@ -27,7 +27,7 @@ def page_count
   @page_count
 end 
 
-def genre - (genre)
+def genre= (genre)
   @genre = genre
 end 
 
